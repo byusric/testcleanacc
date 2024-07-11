f2b82827709ec535205e131aae4fbdd904e0f0c9
Miguel Verdecia <kyljuru@gmail.com>
2024-07-11 10:03:56 -0400

    removed app.vue andd only router view on app

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import '@/presentation/assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import router from './presentation/router'
+import { RouterView } from 'vue-router'
 
-const app = createApp(App)
+const app = createApp(RouterView)
 
 app.use(createPinia())
 app.use(router)
